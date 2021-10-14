b7d5b8e7df0beaaea8a34220d8e16b874bbe1382
yigitaksoy <yigitaksoy@gmail.com>
2021-10-14 15:18:47 +0200

    Add basic checkout functionality

--- rebel_beans/settings.py
+++ rebel_beans/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -183,4 +183,6 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Stripe
 
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
