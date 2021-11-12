ad9963b64ce197801f6774fd4aafb6ff30d39931
yigitaksoy <yigitaksoy@gmail.com>
2021-11-12 11:32:00 +0100

    Add About page urls, views, image and template

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+    
+def about(request):
+    """ A view to return the about page """
+    
+    return render(request, 'home/about.html')
